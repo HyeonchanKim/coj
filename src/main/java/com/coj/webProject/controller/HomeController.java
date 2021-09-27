d25bc3043eb5c0b7202f2164c2c5599ea49e9fb3
junza301 <junza301@naver.com>
2021-09-27 19:18:35 +0900

    bootstrap test

--- src/main/java/com/coj/webProject/controller/HomeController.java
+++ src/main/java/com/coj/webProject/controller/HomeController.java
@@ -1,15 +1,37 @@
 package com.coj.webProject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coj.webProject.data.CojUser;
+import com.coj.webProject.service.CojUserService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private CojUserService cojUserService;
 
 	@GetMapping("/")
 	public String homeView() {
 		return "home";
+	}
+	
+	@GetMapping("/sign")
+	public String sign_in(Model model) {
+		return "sign_in";
+	}
+	
+	@GetMapping("/header")
+	public String header(Model model) {
+		List<CojUser> list = cojUserService.findAll();
+		model.addAttribute("list", list);
+		return "header";
 	}
 }
